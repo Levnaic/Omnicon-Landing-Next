1816b1c354b2033a321c5aec016f809497271e0e
Levnaic <srdjanlevnaic@gmail.com>
2025-11-04 18:20:36 +0100

    ubacen siper carousel

--- src/components/section/features/features.tsx
+++ src/components/section/features/features.tsx
@@ -4,6 +4,7 @@ import "./features.scss";
 import { motion } from "framer-motion";
 import { useFeatures } from "@/hooks/useContent";
 import { Section, RenderIcon, Video } from "@/components";
+import Carousel from "@/components/ui/carousel/carousel";
 import { useState } from "react";
 
 const FeaturesSection = () => {
@@ -29,30 +30,7 @@ const FeaturesSection = () => {
         <p className="features-subtitile">{subtitle}</p>
         <div className="features-presentation">
           <div className="features-select">
-            {featuresList.map((feature) => {
-              return (
-                <article
-                  key={feature.id}
-                  className={`features-item ${
-                    selectedFeatureId === feature.id
-                      ? "features-item-active"
-                      : ""
-                  }`}
-                  onClick={() => setSelectedFeatureId(feature.id)}
-                >
-                  <div className="features-item-upper-part">
-                    <div className="features-item-icon-container">
-                      {RenderIcon({
-                        iconName: feature.icon,
-                        className: "features-item-icon",
-                      })}
-                    </div>
-                    <h3 className="features-item-title">{feature.title}</h3>
-                  </div>
-                  <p>{feature.description}</p>
-                </article>
-              );
-            })}
+            <Carousel />
           </div>
           <div className="features-video-container">
             <Video
